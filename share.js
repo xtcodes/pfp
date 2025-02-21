2f7e2d6934edf21cf10d3c192893a40d6489acb2
Ferry Ayunda <ayunda.fa0604@gmail.com>
2025-02-22 02:12:06 +0700

    Create share.js

---
+++ share.js
@@ -0,0 +1,28 @@
+document.getElementById("shared").addEventListener("click", () => {
+alert("Bagikan Twibbon ini ke media sosial Anda?");
+
+const dataUrl = canvas.toDataURL(); 
+fetch(dataUrl)
+.then(res => res.blob())
+.then(blob => {
+//console.log(blob)
+const filesArray = [new File([blob], 'image.png', { type: blob.type, lastModified: new Date().getTime() })];
+console.log(filesArray);
+const shareData = {
+title: "Twibbon Generator",
+text: "This is a website where you can add photos to frames.",
+url: "https://xtcodes.github.io/pfp/",
+files: filesArray
+};
+console.log(shareData);
+if (navigator.share) {
+navigator.share(
+shareData
+)
+.then(() => alert("Terima kasih telah berbagi.")) 
+.catch(error => alert("Bagikan dibatalkan!", error)); 
+} else {
+alert('navigator.share not available'); 
+}
+})
+});
